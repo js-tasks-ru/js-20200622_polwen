8d86946b519b3eb2170e46ef9737a1b6e4fe3a4d
Sergey Zheleznikov <polwen@yandex.ru>
2020-06-26 17:23:51 +0300

    решил задачу по сортировке строки

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,31 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'lowerAsc') {
 
+const russianAlphabet = 'АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя'.split('');
+const englishAlphabet = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'.split('');
+const bothAlphabets = englishAlphabet.concat(russianAlphabet);
+
+export function sortStrings(arr, param = 'lowerAsc') {
+  const arrToSort = arr.slice();
+  return param === 'desc' ? arrToSort.sort(compare).reverse() : arrToSort.sort(compare);
 }
+
+
+function compare(word1, word2) {
+  const numbersOfLettersInWord1 = transformToArrayOfNumbers(word1);
+  const numbersOfLettersInWord2 = transformToArrayOfNumbers(word2);
+  const length = word1.length > word2.length ? word1.length : word2.length;
+
+  for (let i = 0; i < length; i++) {
+    if (numbersOfLettersInWord1[i] < numbersOfLettersInWord2[i] || numbersOfLettersInWord1[i] === undefined) {
+      return -1;
+    }
+
+    if (numbersOfLettersInWord1[i] > numbersOfLettersInWord2[i] || numbersOfLettersInWord2[i] === undefined) {
+      return 1;
+    }
+  }
+}
+
+const transformToArrayOfNumbers = (word) => word.split('').map((letter) => bothAlphabets.indexOf(letter));
